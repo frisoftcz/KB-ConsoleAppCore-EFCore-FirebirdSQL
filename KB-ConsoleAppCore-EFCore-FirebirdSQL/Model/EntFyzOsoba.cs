51d9c8518930b6a938d7908be130ec65ea577435
Zajic Pavel <zajic@frisoft.cz>
2021-01-10 00:44:41 +0100

    Add Model

---
+++ KB-ConsoleAppCore-EFCore-FirebirdSQL/Model/EntFyzOsoba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KB_ConsoleAppCore_EFCore_FirebirdSQL.Model
+{
+  public class EntFyzOsoba
+  {
+    public string ID { get; set; }
+    public DateTime DATETIME { get; set; }
+    public string LOGIN { get; set; }
+    public string OSOBATYP_ID { get; set; }
+    public string FYZOSOBA_PRIJMENI { get; set; }
+    public string FYZOSOBA_JMENO { get; set; }
+    public DateTime? FYZOSOBA_DAT_NAR { get; set; }
+    public string POZNAMKA { get; set; }
+  }
+}
---
+++ KB-ConsoleAppCore-EFCore-FirebirdSQL/Model/FbDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace KB_ConsoleAppCore_EFCore_FirebirdSQL.Model
+{
+  public class FbDbContext : DbContext
+  {
+    public DbSet<EntFyzOsoba> FyzOsoba { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseFirebird(@"database=localhost:C:\DotNetCoreProjects\Console_EF_FB_1\Console_EF_FB_1\FBDatabase\FB_Database_1.FDB;user=sysdba;password=masterkey");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity("KB_ConsoleAppCore_EFCore_FirebirdSQL.Model.EntFyzOsoba", b =>
+      {
+        b.Property("ID").HasColumnName("ID");
+        b.Property("DATETIME").HasColumnName("DATETIME").IsConcurrencyToken();
+        b.Property("LOGIN").HasColumnName("LOGIN");
+        b.Property("OSOBATYP_ID").HasColumnName("OSOBATYP_ID");
+        b.Property("FYZOSOBA_PRIJMENI").HasColumnName("FYZOSOBA_PRIJMENI");
+        b.Property("FYZOSOBA_JMENO").HasColumnName("FYZOSOBA_JMENO");
+        b.Property("FYZOSOBA_DAT_NAR").HasColumnName("FYZOSOBA_DAT_NAR");
+        b.Property("POZNAMKA").HasColumnName("FYZOSOBA_POZN");
+        b.HasKey("ID");
+        b.ToTable("V_FYZ_OSOBA");
+      });
+    }
+  }
+}
