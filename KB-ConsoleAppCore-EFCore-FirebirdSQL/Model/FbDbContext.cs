350c3bb48bcf73267e3214dac60c90374c1b0782
Zajic Pavel <zajic@frisoft.cz>
2021-01-11 12:09:30 +0100

    Add FbDbContext

--- KB-ConsoleAppCore-EFCore-FirebirdSQL/Model/FbDbContext.cs
+++ KB-ConsoleAppCore-EFCore-FirebirdSQL/Model/FbDbContext.cs
@@ -13,7 +13,7 @@ namespace KB_ConsoleAppCore_EFCore_FirebirdSQL.Model
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-      optionsBuilder.UseFirebird(@"database=localhost:C:\DotNetCoreProjects\Console_EF_FB_1\Console_EF_FB_1\FBDatabase\FB_Database_1.FDB;user=sysdba;password=masterkey");
+      optionsBuilder.UseFirebird(@"datasource=localhost;database=C:\DotNetCoreProjects\Console_EF_FB_1\Console_EF_FB_1\FBDatabase\FB_Database_1.FDB;user=sysdba;password=masterkey");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- KB-ConsoleAppCore-EFCore-FirebirdSQL/Program.cs
+++ KB-ConsoleAppCore-EFCore-FirebirdSQL/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using KB_ConsoleAppCore_EFCore_FirebirdSQL.Model;
 
 namespace KB_ConsoleAppCore_EFCore_FirebirdSQL
 {
@@ -6,7 +9,44 @@ namespace KB_ConsoleAppCore_EFCore_FirebirdSQL
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      using (var db = new FbDbContext())
+      {
+        var data1 = db.Set<EntFyzOsoba>()
+          .ToList();
+        foreach (var item in data1)
+        {
+          Console.WriteLine($"{item.ID}: {item.FYZOSOBA_PRIJMENI} ({item.DATETIME})");
+        }
+
+        var data2 = db.FyzOsoba.ToList();
+        foreach (var item in data2)
+        {
+          Console.WriteLine($"{item.ID}: {item.FYZOSOBA_PRIJMENI} ({item.DATETIME})");
+        }
+
+
+        EntFyzOsoba fyzOsoba = db.FyzOsoba
+          .Where(x => x.ID == "20181111180000000001")
+          .FirstOrDefault<EntFyzOsoba>();
+
+        Console.ReadLine();
+
+        try
+        {
+          fyzOsoba.FYZOSOBA_PRIJMENI = "Vomáčka";
+          fyzOsoba.DATETIME = DateTime.Now;
+          db.SaveChanges();
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+          Console.WriteLine("Záznam byl změěnn jiným uživatelem.");
+          foreach (var entry in ex.Entries)
+          {
+            Console.WriteLine(entry.Metadata.Name);
+          }
+        }
+      }
+      Console.ReadLine();
     }
   }
 }
